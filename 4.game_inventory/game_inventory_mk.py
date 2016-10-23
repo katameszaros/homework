a6d0ac3e64ab6b90e452fb5b061d7feba0ab0c17
Kata Meszaros <kata.meszaros92@gmail.com>
2016-10-23 11:24:36 +0200

    step 2

--- 4.game_inventory/game_inventory_mk.py
+++ 4.game_inventory/game_inventory_mk.py
@@ -1,4 +1,6 @@
+
 inv = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12} 
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 
 def display_inventory():
     print("Inventory:")
@@ -6,5 +8,17 @@ def display_inventory():
         print(value, key)
     print("Total number of items: " + str(sum(inv.values())))
 
+def add_to_inventory(inventory, added_items):
+    for i in added_items:
+        if i in inventory.keys():
+            count = inventory.get(i)
+            count += 1
+            inventory[i] = count
+        else:
+            inventory[i]=1
+    return inventory
+
+inv = add_to_inventory(inv, dragon_loot)
 
 display_inventory()
+
